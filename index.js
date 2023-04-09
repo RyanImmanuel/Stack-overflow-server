f1e1e7c0a745ef2a179cf4af34e6c162116a4f1c
Ryan Immanuel <107943648+RyanImmanuel@users.noreply.github.com>
2023-04-09 18:49:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("This is a stack overflow clone API model");
+  res.send("This is a stack overflow clone API");
 });
 
 app.use("/user", userRoutes);
